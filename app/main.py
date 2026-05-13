1b84e7d7aea030c942c7b6aeea83d40769dd0e2a
com1rr <1396405397@qq.com>
2026-05-13 15:28:33 +0800

    测试Docker layer cache

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello DevOps CI/CD Demo v2 from GitHub Actions!"
+    return "Hello DevOps CI/CD Demo v3 from GitHub Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

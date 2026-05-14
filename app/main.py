5bbea532524d3918c6ae9fe402517e18cc1d3b48
com1rr <1396405397@qq.com>
2026-05-14 21:07:40 +0800

    更新首页返回消息为“Hello DevOps from prod environment!”

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello DevOps from dev environment!"
+    return "Hello DevOps from prod environment!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
